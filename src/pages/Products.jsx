aa8b28d5ef0638ff19534083cd31b819d94e8907
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-15 17:27:17 +0200

    [UPD] made product iteration with map and add differente fields - Esercizio Terminato

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,7 +5,7 @@ import MainNavbar from '../components/MainNavbar'
 
 const Products = () => {
 
-  const [products, setProducts] = useState()
+  const [products, setProducts] = useState([])
 
   const getProduct = () => {
     axios.get('https://fakestoreapi.com/products')
@@ -26,8 +26,22 @@ const Products = () => {
             <div className='col-12'>
               <h1>La lista dei nostri prodotti</h1>
             </div>
-            <div className="col-12 col-md-6 col-lg-4">
-
+            <div className="row">
+              {products.map((product) => (
+                  <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                      <div className="card-image-top">
+                        <img src={product.image} className='img-fluid' alt="" />
+                      </div>
+                      <div className="card-body">
+                        <h3>{product.title}</h3>
+                        <p>{product.description}</p>
+                        <p>{product.price}</p>
+                      </div>
+                    </div>
+                  </div>
+                )
+              )};
             </div>
           </div>
         </div>
