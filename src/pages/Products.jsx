8057291e502854254bf0fb87c914f1d3a5a084f0
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-15 16:58:12 +0200

    [UPD] import axios and add cols and rows for products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+
 import MainNavbar from '../components/MainNavbar'
 
 function Products() {
   return (
     <>
         <MainNavbar />
-        <div>Products</div>
+        <div className='container'>
+          <div className="row">
+            <div className='col-12'>
+              <h1>La lista dei nostri prodotti</h1>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4">
+              
+            </div>
+          </div>
+        </div>
     </>
   )
 }
