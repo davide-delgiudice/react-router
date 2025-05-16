1d886502abea48ee4c429b6ed96bef8423860cc6
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-16 16:58:08 +0200

    [UPD] add singleProduct state and create getSingleProduct function

--- src/pages/products/DetailProduct.jsx
+++ src/pages/products/DetailProduct.jsx
@@ -7,6 +7,15 @@ import { useParams } from 'react-router-dom'
 const DetailProduct = () => {
 
   const { id } = useParams();
+  // variabile di stato che conterrà il prodotto
+  const [singleProduct, setSingleProduct] = useState([]);
+
+  // funzione che recupera il prodotto
+  const getSingleProduct = () => {
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) => {
+        setSingleProduct(resp.data);
+    })
+  };
 
   return (
     <div>DetailProduct</div>
