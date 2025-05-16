588267ee4d3d3a922dec70e28c9c8a44c40453b8
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-16 16:30:01 +0200

    [UPD] import useParams and add const destructure

--- src/pages/products/DetailProduct.jsx
+++ src/pages/products/DetailProduct.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const DetailProduct = () => {
+
+  const { id } = useParams();
+
   return (
     <div>DetailProduct</div>
   )
